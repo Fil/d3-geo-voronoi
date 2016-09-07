1dc195c44bd67e174f56bab0b6bf18347fcd59c5
Fil <fil@rezo.net>
2016-09-07 23:34:58 +0200

    some tests

--- test/geo-voronoi-test.js
+++ test/geo-voronoi-test.js
@@ -1,7 +1,20 @@
 var tape = require("tape"),
-    foo = require("../");
+    geoVoronoi = require("../");
 
-tape("foo() returns the answer to the ultimate question of life, the universe, and everything.", function(test) {
-  test.equal(foo.foo(), 42);
+tape("geoVoronoi() returns a Diagram.", function(test) {
+  test.equal(typeof geoVoronoi, 'object');
+  test.equal(typeof geoVoronoi.geoVoronoi, 'function');
+  test.equal(typeof geoVoronoi.geoVoronoi(), 'function');
+  test.equal(typeof geoVoronoi.geoVoronoi([]), 'function');
+  test.equal(typeof geoVoronoi.geoVoronoi().links, 'function');
+  test.equal(typeof geoVoronoi.geoVoronoi().links([]), 'object');
+  test.equal(typeof geoVoronoi.geoVoronoi().polygons, 'function');
+  test.equal(typeof geoVoronoi.geoVoronoi().polygons([]), 'object');
+  test.equal(typeof geoVoronoi.geoVoronoi().triangles, 'function');
+  test.equal(typeof geoVoronoi.geoVoronoi().triangles([]), 'object');
+  test.equal(typeof geoVoronoi.geoVoronoi().urquhart, 'function');
+  test.equal(typeof geoVoronoi.geoVoronoi().urquhart([]), 'object');
+  test.equal(typeof geoVoronoi.geoVoronoi().circumcenters, 'function');
+  test.equal(typeof geoVoronoi.geoVoronoi().circumcenters([]), 'object');
   test.end();
 });
