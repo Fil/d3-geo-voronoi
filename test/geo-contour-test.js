d0968eaf5954f25814da24ccaf46ff21db76a170
Philippe Rivière <fil@rezo.net>
2020-05-03 21:47:32 +0200

    no random in tests

--- test/geo-contour-test.js
+++ test/geo-contour-test.js
@@ -2,6 +2,7 @@ var fs = require('fs');
 var path = require('path');
 var tape = require("tape");
 var geoVoronoi = require("../");
+var d3 = require("d3-geo");
 
 tape("geoContour() returns a contour generator.", function(test) {
   test.equal(typeof geoVoronoi, 'object');
@@ -17,12 +18,14 @@ tape("geoContour() computes something.", function(test) {
 });
 
 tape("geoContour() computes something 2.", function(test) {
-  var sites = Array.from({ length: 500 }, () => [
-    360 * Math.random() - 180,
-    90 * (Math.random() - Math.random()),
-    Math.random()
+  var sites = Array.from({ length: 500 }, (_,i) => [
+    180 * Math.sin(i),
+    90 * Math.cos(i * 7/11),
+    i
   ]);
   var u = geoVoronoi.geoContour()(sites);
   test.ok(u);
+  var areas = u.map(d => Math.round(100 * d3.geoArea(d)) / 100);
+  test.deepEqual(areas, [ 0, 12.36, 11.41, 9.58, 7.57, 5.78, 4.18, 2.93, 1.8, 0.39, 0 ]);
   test.end();
 });
